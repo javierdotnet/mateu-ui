946a61697df9a2bfede73cd47499de5027414dc0
miguelperezcolom <miguelperezcolom@gmail.com>
2016-08-08 21:11:42 +0200

    primer commit

---
+++ core/src/main/java/mateu/ui/core/app/AbstractModule.java
@@ -0,0 +1,7 @@
+package mateu.ui.core.app;
+
+/**
+ * Created by miguel on 8/8/16.
+ */
+public class AbstractModule {
+}
---
+++ core/src/main/java/mateu/ui/core/components/AbstractContainer.java
@@ -0,0 +1,7 @@
+package mateu.ui.core.components;
+
+/**
+ * Created by miguel on 8/8/16.
+ */
+public class AbstractContainer {
+}
